410a1acbc1b5b1dca836d867bd5a60438092fc8d
lutvs <lutfi.zikri@gmail.com>
2026-08-25 21:50:10 +0700

    Add files via upload

---
+++ autoCopyzip.bat
@@ -0,0 +1,24 @@
+@ECHO off  
+  
+@REM add adb.exe to PATH
+ECHO initialize...
+
+@SET PATH=%PATH%;%CD%\Adb  
+@REM SET PATH  
+ECHO PATH
+  
+@REM label of infinite loop  
+:LOOP  
+  
+ECHO waiting for insertion cellphone... 
+adb wait-for-device    
+@REM circulation install this catalogue's APP 
+FOR %%i IN (*.obb) DO (   
+    ECHO copying:%%i  
+    adb push "%%i" /sdcard/install
+    )  
+  
+ECHO copy Finish: opo nang ?!!!
+PAUSE
+GOTO LOOP 
+@ECHO on 
